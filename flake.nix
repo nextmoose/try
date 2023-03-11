d10dd42753521a496f3c5ad41a1ce401fd7a2b98
Emory Merryman <emory.merryman@gmail.com>
2023-03-11 14:58:58 -0500

    restart

---
+++ flake.nix
@@ -0,0 +1,23 @@
+  {
+      inputs =
+        {
+          flake-utils.url = "github:numtide/flake-utils" ;
+        } ;
+      outputs =
+        { self , flake-utils } :
+          flake-utils.lib.eachDefaultSystem
+          (
+            system :
+              {
+	        lib =
+		  try :
+		    let
+		      trial =
+		        seed :
+			  let
+			    eval = try seed ;
+			    in if eval.success then eval.value else trial ( seed + 1 ) ;
+		       in trial 0 ;
+              }
+      ) ;
+    }
